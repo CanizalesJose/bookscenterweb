438aefd42ef755263499a8d9acc62e6ab9b65e7d
CanizalesJose <jose1605m@gmail.com>
2024-09-26 12:35:36 -0700

    Agregados y actualizados componentes

---
+++ src/components/BookCardComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col s3">
+        <div class="card">
+            <div class="card-image">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s">
+                <span class="card-title black-text">Titulo del libro</span>
+            </div>
+            <div class="card-content">
+                <p>
+                    libro
+                </p>
+            </div>
+            <div class="card-action">
+                <a class="btn waves-effect waves-purple" href="#">Agregar</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card-image{
+    width: 100%;
+    min-width: 200px;
+}
+.card{
+    min-width: 200px;
+}
+</style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,4 +1,11 @@
 <template>
+    <ul id="adminMenu" class="dropdown-content">
+        <li><a href="#">Usuarios</a></li>
+        <li><a href="#">Categorias</a></li>
+        <li><a href="#">Autores</a></li>
+        <li><a href="#">Libros</a></li>
+        <li><a href="#">Prestamos</a></li>
+    </ul>
     <div class="navbar-fixed">
         <nav class="custom-nav">
             <div class="nav-wrapper brown lighten-2">
@@ -11,11 +18,19 @@
                 <ul class="left hide-on-med-and-down">
                     <!-- Lista con los apartados de la página -->
                     <li class="hoverable"><a href="/">Inicio</a></li>
-                    <li class="hoverable"><a href="#">Catalogo</a></li>
-                    <li class="hoverable"><a href="#">Acerca de...</a></li>
+                    <li class="hoverable"><a href="/catalog">Catalogo</a></li>
+                    <li class="hoverable"><a href="/about">Acerca de...</a></li>
+                    <li v-if="usertype=='admin'" class="hoverable">
+                        <a class="dropdown-trigger" data-target="adminMenu">
+                            Administración<i class="material-icons right">arrow_drop_down</i>
+                        </a>
+                    </li>
                 </ul>
                 <ul class="right hide-on-med-and-down">
-                    <li class="hoverable"><a href="/login">Iniciar Sesión</a></li>
+                    <li v-if="username" style="padding-right: 30px;">{{ username }}</li>
+                    <li v-if="usertype">{{ usertype }}</li>
+                    <li v-else class="hoverable"><a href="/login">Iniciar Sesión &nbsp;&nbsp;&nbsp;</a></li>
+                    <li v-if="usertype && username" style="padding-right: 30px; padding-left: 30px;" class="red-text"><a href="/logout">Cerrar Sesión</a></li>
                 </ul>
             </div>
         </nav>
@@ -23,11 +38,33 @@
 
     <ul class="sidenav" id="mobile-demo">
         <li><a href="/">Inicio</a></li>
-        <li><a href="#">Catalogo</a></li>
-        <li><a href="#">Acerca de...</a></li>
-        <li><a href="/login">Iniciar sesión</a></li>
+        <li><a href="/catalog">Catalogo</a></li>
+        <li><a href="/about">Acerca de...</a></li>
+        <li v-if="username" style="padding-top: 30px;"><a>{{ username }}</a></li>
+        <li v-if="usertype"><a>{{ usertype }}</a></li>
+        <li v-else><a href="/login">Iniciar sesión</a></li>
+        <li v-if="username && usertype" style="padding-top: 30px;"><a href="/logout">Cerrar Sesión</a></li>
     </ul>
 </template>
+
+<script setup>
+    import {ref, onMounted} from 'vue';
+    /* eslint-disable */
+    const username = ref(null);
+    const usertype = ref(null);
+
+    onMounted(() =>{
+        let storedToken = localStorage.getItem('token');
+        const storedUsername = localStorage.getItem('username');
+        const storedUsertype = localStorage.getItem('usertype');
+
+        if (storedToken && storedUsername && storedUsertype){
+            username.value = storedUsername;
+            usertype.value = storedUsertype;
+        }
+    });
+
+</script>
 
 <style scoped>
     .custom-nav {
