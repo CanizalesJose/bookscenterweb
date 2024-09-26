ef2afdb56670b1fff56c87c85ee7db95d46216d8
CanizalesJose <jose1605m@gmail.com>
2024-09-25 20:33:39 -0700

    Actualizado

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,52 @@
 <template>
     <div>
         <HeaderComponent/>
-        <h1>Bienvenido a </h1>
-        <p>Texto de prueba en home</p>
-        <br>
+        <div class="section">
+            <div class="container center">
+                <br><br>
+                <h3 class="header">Bienvenido a BookCenter</h3>
+                <br>
+                <div class="divider"></div>
+                <br>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc diam nisl, tristique non enim ac, ultrices fringilla nibh. Vivamus sodales mi turpis, vel ultrices enim viverra id. Proin pharetra, turpis ut commodo commodo, turpis dui pretium dui, sed interdum sapien neque sed libero. Praesent aliquam risus arcu, vel rhoncus ante porta id. Sed imperdiet mi ut lacus tincidunt tristique. Morbi laoreet nibh sit amet massa elementum, imperdiet porttitor justo aliquam. Nullam vel posuere magna, vitae venenatis orci. Sed finibus, justo vel pellentesque ullamcorper, dui ante imperdiet ex, id pellentesque massa felis eu elit. Donec vel dui posuere sem scelerisque tincidunt bibendum in urna.
+                </p>
+                <p>
+                    Maecenas metus ex, consequat vitae risus egestas, tristique semper purus. Nam facilisis metus sed nisl auctor aliquet. Donec egestas leo ac magna sodales, ac vehicula ipsum malesuada. Vestibulum pellentesque aliquet augue semper elementum. Nulla sit amet cursus tortor, id gravida turpis. Ut eu rhoncus erat, nec tempus tellus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Etiam dictum magna ac felis maximus semper. Curabitur tempus, risus quis sollicitudin luctus, mauris tortor ultrices dui, eget imperdiet leo quam a odio. Nullam quis pellentesque metus. Ut commodo molestie nisl. Vivamus aliquam tristique consequat. Vivamus non massa lobortis, cursus ligula non, aliquam nunc. Duis dictum mi pellentesque nisl pharetra, quis luctus magna fermentum. Nulla semper, sem at consectetur laoreet, nisl dolor luctus ipsum, vitae vehicula mauris velit at nibh.
+                </p>
+                
+                <div class="divider"></div>
+                <h5 class="header">Top Lecturas del mes</h5>
+                <div class="carousel">
+                    <a class="carousel-item" href="#1">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s">
+                    </a>
+
+                    <a class="carousel-item" href="#2">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s">
+                    </a>
+
+                    <a class="carousel-item" href="#3">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s">
+                    </a>
+
+                    <a class="carousel-item" href="#4">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s">
+                    </a>
+
+                    <a class="carousel-item" href="#5">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58P55blSKZmf2_LdBoU7jETl6OiB2sjYy9A&s">
+                    </a>
+                </div>
+
+                <p>
+                    Donec in sem in libero viverra molestie. Quisque at massa vehicula, consectetur massa in, facilisis urna. Ut sit amet tristique dui. Aenean fringilla nibh at dui mollis, non pharetra magna convallis. Aliquam et volutpat turpis. In vehicula venenatis nisi vitae volutpat. Cras porttitor ligula vel arcu tincidunt condimentum. Curabitur lacinia viverra elit, non ornare enim pretium vel. Nullam tincidunt arcu vitae metus egestas, ut pulvinar ex feugiat. In at rhoncus odio, et tempus nibh. Aenean at fermentum felis, non cursus elit. Nulla tincidunt nec urna non tincidunt.
+                </p>
+                <p>
+                    Suspendisse risus turpis, faucibus nec tristique at, faucibus nec dolor. Vivamus et ultricies enim, id imperdiet elit. Morbi non tincidunt lectus. Nunc volutpat eros a lorem pellentesque, eleifend porta libero euismod. Vestibulum nec fermentum nisl, id hendrerit erat. Vivamus sed pharetra arcu. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Cras tincidunt dignissim ornare. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse potenti. Praesent vitae dignissim felis. Phasellus odio justo, blandit a iaculis in, convallis vel mauris. Maecenas auctor vel risus in elementum. Fusce ut accumsan nulla. Fusce bibendum justo ac lorem volutpat dictum.
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,107 @@
 <template>
 <div>
     <HeaderComponent></HeaderComponent>
-    <h1>Login</h1>
-    <form>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username">
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password">
-        <button type="submit">Login</button>
-    </form>
+    <!-- Crear un contenedor alineado al centro -->
+    <div class="valign-wrapper loginContainer">
+        <div class="container center-align brown">
+
+            <!-- Generar un renglon centrado -->
+            <div class="row center">
+                <!-- Espacio en blanco de dos columnas para margen -->
+                <div class="col s2"></div>
+
+
+                <!-- Formulario en el centro -->
+                <form class="col s8" action="javascript:void(0);">
+                    <!-- Margen superior del formulario -->
+                    <div class="row"><br></div>
+                    
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">account_circle</i>
+                            <input v-model="username" id="username" name="username" type="text" class="validate" data-length="30" required>
+                            <label for="username">Username</label>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <i class="material-icons prefix">lock</i>
+                            <input v-model="password" placeholder="Password" id="password" name="password" type="password" class="validate" data-length="16" required>
+                            <label for="password">Password</label>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <button class="btn waves-effect waves-light" @click="valInput">
+                                Iniciar Sesión
+                                <i class="material-icons right">send</i>
+                            </button>
+                        </div>
+                    </div>
+
+                    <!-- Margen inferior del formulario -->
+                    <div class="row"><br></div>
+                </form>
+
+
+                <div class="col s2"></div>
+
+            </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <script setup>
+    /* eslint-disable */
     import HeaderComponent from '@/components/HeaderComponent.vue';
+    import { useRouter } from 'vue-router';
+    import axios from 'axios';
+    import { ref } from 'vue'
+    const router = useRouter();
+
+    const username = ref('');
+    const password = ref('');
+    async function valInput() {
+        if (username.value.length != 0 && password.value.length != 0){
+            try {
+                router.push('/');
+                // let result = await axios.post('http://localhost:5000/api/users/login', {
+                //     username: username.value,
+                //     password: password.value
+                // });
+                // console.log(result);
+            } catch (error) {
+                if (error.response){
+                    console.log(error.response.data.message);
+                    username.value = '';
+                    password.value ='';
+                }
+            }
+        }
+    }
+
 </script>
+
+<style scoped>
+    .loginContainer {
+        padding-top: 5%;
+        padding-bottom: 5%;
+        padding-left: 10%;
+        padding-right: 10%;
+        width: 100%;
+        height: 100%;
+        position: absolute;
+    }
+    .loginContainer .container {
+        width: 50%;
+        height: 70%;
+    }
+
+    .input-field .helper-text {
+        display: block !important;
+    }
+
+</style>
