84386696e89223f2665b12d628d1722735f96f75
CanizalesJose <jose1605m@gmail.com>
2024-10-01 23:09:19 -0700

    Agregadas referencias a estilos locales

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
+import './assets/css/materialize.min.css';
+import './assets/js/materialize.min.js';
 
 const app = createApp(App);
 app.use(router);
