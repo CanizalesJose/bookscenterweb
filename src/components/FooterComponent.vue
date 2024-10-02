5cd50ab4fc8797e54014f88814903bc46e6f7f05
CanizalesJose <jose1605m@gmail.com>
2024-10-01 23:07:11 -0700

    Agregado componente footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="parallax-container">
+        <div class="parallax">
+            <img :src="imageSrc" alt="Footer Parallax Image">
+        </div>
+    </div>
+</template>
+
+<script setup>
+/* eslint-disable */
+/* global M */
+import { onMounted } from 'vue';
+onMounted(() => {
+  const elems = document.querySelectorAll('.parallax');
+  M.Parallax.init(elems);
+});
+const props = defineProps({
+imageSrc: {
+    type: String,
+    required: true
+}
+});
+</script>
+  
+<style scoped>
+.parallax-container {
+    height: 70px;
+}
+
+.parallax img {
+    width: 100%;
+    height: auto;
+}
+</style>
