29a27987b2f57416b1c528417ebd531e3dd107a7
CanizalesJose <jose1605m@gmail.com>
2024-11-01 18:57:26 -0700

    Actualizado diseño

--- src/views/LoanAdminView.vue
+++ src/views/LoanAdminView.vue
@@ -28,58 +28,46 @@
                 <h3>Prestamos pendientes</h3>
             </div>
             <!-- Si no hay prestamos pendientes -->
-            <div v-if="pendingLoansData.length == 0" class="container center">
+            <div v-if="pendingLoans.length == 0" class="container center">
                 <img style="width: 300px;" src="../assets/img/todo-bien.png">
                 <h5>No hay prestamos pendientes</h5>
             </div>
-            <!-- Tablas de prestamos pendientes separados por id de prestamo -->
-            <div v-for="loan in pendingLoansData" :key="loan.reference.id">
-                <div class="row">
-                    <div class="divider"></div>
-                    <div class="col s12 m6 center">
-                        <p style="font-size: 20px;"><b>Pedido:</b> {{ loan.reference.id }}</p>
-                        <p style="font-size: 20px;"><b>Fecha de prestamo: </b>{{ loan.reference.date }}</p>
-                    </div>
-                    <div class="col s12 m6 center">
-                        <p style="font-size: 20px;"><b>Usuario:</b> {{ loan.reference.username }}</p>
-                        <p style="font-size: 20px;"><b>Fecha limite: </b>{{ loan.reference.returnDate }}</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <table class="highlight responsive-table">
-                        <thead>
-                            <tr>
-                                <th>Portada</th>
-                                <th>Titulo</th>
-                                <th>Autor</th>
-                                <th>Categoría</th>
-                                <th>ISBN</th>
-                                <th>Estado</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="book in loan.books" :key="book.bookId">
-                                <td>
-                                    <img :src="book.cover" class="listCover materialboxed">
-                                </td>
-                                <td>{{ book.title }}</td>
-                                <td>{{ book.author }}</td>
-                                <td>{{ book.category }}</td>
-                                <td>{{ book.isbn }}</td>
-                                <td v-if="book.returned==0">
-                                    Pendiente
-                                    <br><br>
-                                    <button @click="changeStatusModal(loan.reference.id, book.bookId, book.cover, book.title, book.author, book.isbn)" class="btn-floating waves-effect-light green lighten-1 center hoverable">
-                                        <i class="material-icons">book</i>
-                                    </button>
-                                </td>
-                                <td v-if="book.returned==1">Entregado</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <br><br>
-            </div>
+            <!-- Tabla de prestamos pendientes -->
+            <table class="responsive-table">
+                <thead>
+                    <tr>
+                        <th><!-- Portada --></th>
+                        <th>Titulo</th>
+                        <th>Prestamo Id.</th>
+                        <th>ISBN</th>
+                        <th>Autor</th>
+                        <th>Estado</th>
+                        <th>Usuario</th>
+                        <th>Ver</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="loan in pendingLoans" :key="'pending'+loan.loanId+loan.bookId">
+                        <td>
+                            <img :src="loan.cover" class="listCover materialboxed" @load="initMaterialBoxed()">
+                        </td>
+                        <td>{{ loan.title }}</td>
+                        <td>{{ loan.loanId }}</td>
+                        <td>{{ loan.isbn }}</td>
+                        <td>{{ loan.author }}</td>
+                        <td>
+                            <i v-if="loan.returned == 1" class="material-icons">check</i>
+                            <i v-if="loan.returned == 0" class="material-icons">alarm</i>
+                        </td>
+                        <td>{{ loan.username }}</td>
+                        <td>
+                            <button @click="selectBook(loan.loanId, loan.bookId, loan.username, loan.contactNumber, loan.email, loan.date, loan.returnDate, loan.title, loan.author, loan.cover, loan.pending, loan.isbn)" class="btn-floating black modal-trigger" data-target="changeStatusModal">
+                                <i class="material-icons">search</i>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
     <!-- Imagen -->
@@ -95,59 +83,39 @@
                 <h3>Historial de prestamos</h3>
             </div>
             <!-- Tabla de historial -->
-            <div v-for="loan in returnedLoansData" :key="loan.reference.id">
-                <div class="row">
-                    <div class="divider"></div>
-                    <div class="col s12 m6 center">
-                        <p style="font-size: 20px;"><b>Pedido:</b> {{ loan.reference.id }}</p>
-                        <br>
-                        <p style="font-size: 20px;"><b>Fecha de prestamo: </b>{{ loan.reference.date }}</p>
-                    </div>
-                    <div class="col s12 m6 center">
-                        <p style="font-size: 20px;"><b>Usuario:</b> {{ loan.reference.username }}</p>
-                        <br>
-                        <p style="font-size: 20px;"><b>Fecha limite: </b>{{ loan.reference.returnDate }}</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <table class="highlight responsive-table">
-                        <thead>
-                            <tr>
-                                <th>Portada</th>
-                                <th>Titulo</th>
-                                <th>Autor</th>
-                                <th>Categoría</th>
-                                <th>ISBN</th>
-                                <th>Estado</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="book in loan.books" :key="book.bookId">
-                                <td>
-                                    <img :src="book.cover" class="listCover materialboxed">
-                                </td>
-                                <td>{{ book.title }}</td>
-                                <td>{{ book.author }}</td>
-                                <td>{{ book.category }}</td>
-                                <td>{{ book.isbn }}</td>
-                                <td v-if="book.returned==0">
-                                    Pendiente
-                                    <br><br>
-                                    <button @click="changeStatusModal(book.bookId, loan.reference.id)" class="btn-floating waves-effect-light green lighten-1 center">
-                                        <i class="material-icons prefix">box_add</i>
-                                    </button>
-                                </td>
-                                <td v-if="book.returned==1">Entregado</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <br><br>
-            </div>
+            <table class="responsive-table">
+                <thead>
+                    <tr>
+                        <th><!-- Portada --></th>
+                        <th>Titulo</th>
+                        <th>Prestamo Id.</th>
+                        <th>ISBN</th>
+                        <th>Autor</th>
+                        <th>Estado</th>
+                        <th>Usuario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="loan in returnedLoans" :key="'returned'+loan.loanId+loan.bookId">
+                        <td>
+                            <img :src="loan.cover" class="listCover materialboxed" @load="initMaterialBoxed()">
+                        </td>
+                        <td>{{ loan.title }}</td>
+                        <td>{{ loan.loanId }}</td>
+                        <td>{{ loan.isbn }}</td>
+                        <td>{{ loan.author }}</td>
+                        <td>
+                            <i v-if="loan.returned == 1" class="material-icons">check</i>
+                            <i v-if="loan.returned == 0" class="material-icons">alarm</i>
+                        </td>
+                        <td>{{ loan.username }}</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
     <!-- Si no hay registro -->
-    <div v-if="returnedLoansData.length == 0" class="container center">
+    <div v-if="returnedLoans.length == 0" class="container center">
         <img style="width: 300px;" src="../assets/img/todo-bien.png">
         <h5>No hay registro por mostrar</h5>
     </div>
@@ -157,6 +125,20 @@
             <h4>¿Confirmar entrega?</h4>
             <div class="divider"></div>
             <p>Solo presiona confirmar si estas seguro de querer cambiar el estado del libro prestado, ten en cuenta que no puede deshacerse.</p>
+            <div class="row">
+                <p><b>Id del Prestamo</b> - {{ selBook.loanId }}</p>
+            </div>
+            <div class="row">
+                <div class="col s6 left-align">
+                    <p><b>Fecha del prestamo</b> - {{ selBook.date }}</p>
+                    <p><b>Pedido por</b> - {{ selBook.username }}</p>
+                    <p><b>Correo:</b> {{ selBook.email }}</p>
+                </div>
+                <div class="col s6 right-align">
+                    <p>{{ selBook.returnDate }} - <b>Fecha de entrega</b></p>
+                    <p>{{ selBook.contactNumber }} - <b>Número</b></p>
+                </div>
+            </div>
             <table class="highlight responsive-table">
                 <thead>
                     <tr>
@@ -169,11 +151,11 @@
                 <tbody>
                     <tr>
                         <td>
-                            <img :src="selBookCover" class="listCover materialboxed">
+                            <img :src="selBook.cover" class="listCover materialboxed">
                         </td>
-                        <td>{{ selBookTitle }}</td>
-                        <td>{{ selBookAuthor }}</td>
-                        <td>{{ selBookIsbn }}</td>
+                        <td>{{ selBook.title }}</td>
+                        <td>{{ selBook.author }}</td>
+                        <td>{{ selBook.isbn }}</td>
                     </tr>
                 </tbody>
             </table>
@@ -181,34 +163,41 @@
         <div class="modal-footer">
             <div class="divider"></div>
             <a class="modal-close waves-effect waves-red btn-flat">Cancelar</a>
-            <a @click="confirmChangeStatus" class="modal-close waves-effect waves-green btn-flat">Confirmar Entrega</a>
+            <a @click="confirmChangeStatus" class="waves-effect waves-light btn-flat">Confirmar Entrega</a>
         </div>
     </div>
 </template>
 
 <script setup>
 /* global M */
+
 import HeaderComponent from '@/components/HeaderComponent.vue';
 import axios from 'axios';
 import { inject, onMounted, ref } from 'vue';
-
+/* eslint-disable */
 const verifyAdmin = inject('verifyAdmin');
-const returnedLoans = ref([]);
-const returnedLoansData = ref([]);
 const pendingLoans = ref([]);
-const pendingLoansData = ref([]);
-const selLoanId = ref(null);
-const selBookId = ref(null);
-const selBookTitle = ref(null);
-const selBookCover = ref(null);
-const selBookAuthor = ref(null);
-const selBookIsbn = ref(null);
+const returnedLoans = ref([]);
+const selBook = ref({
+    loanId: null,
+    bookId: null,
+    username: null,
+    contactNumber: null,
+    email: null,
+    date: null,
+    returnDate: null,
+    title: null,
+    author: null,
+    cover: null,
+    pending: null,
+    isbn: null
+});
 
 onMounted(async () => {
     if (!await verifyAdmin())
         return;
-    await fetchReturnedLoans();
-    await fetchPendingLoans();
+    await fetchPending();
+    await fetchReturned();
     await initMaterialize();
 });
 
@@ -217,91 +206,73 @@ async function initMaterialize() {
     M.Parallax.init(parallaxElems);
     const modalElems = document.querySelectorAll('.modal');
     M.Modal.init(modalElems);
+}
+function initMaterialBoxed(){
     const materialBoxedElems = document.querySelectorAll('.materialboxed');
     M.Materialbox.init(materialBoxedElems);
 }
-
-async function fetchReturnedLoans(){
-    returnedLoansData.value = [];
-    await axios.get(`${process.env.VUE_APP_API_URL}/loans/fetchReturned`, {
+async function fetchPending() {
+    pendingLoans.value = [];
+    axios.get(`${process.env.VUE_APP_API_URL}/loans/fetchPending`, {
         headers: {
             token: localStorage.getItem('token')
         }
     })
-    .then(async res => {
-        returnedLoans.value = res.data;
-        for (const loan of returnedLoans.value) {
-            let newData = await axios.get(`${process.env.VUE_APP_API_URL}/loans/fetchById/${loan.id}`,{
-                headers: {
-                    token: localStorage.getItem('token')
-                }
-            })
-            .then(res => {
-                return res.data;
-            });
-            returnedLoansData.value.push({reference: loan, books: newData});
-        }
-    })
-    .catch(error => {
-        M.toast({html: `Error en la solicitud: ${error.response.data.message}`, classes: 'yellow darken-4'});
-    });
-}
-async function fetchPendingLoans(){
-    pendingLoansData.value = [];
-    await axios.get(`${process.env.VUE_APP_API_URL}/loans/fetchPending`, {
-        headers: {
-            token: localStorage.getItem('token')
-        }
-    })
-    .then(async res => {
+    .then(res => {
         pendingLoans.value = res.data;
-        for (const loan of pendingLoans.value) {
-            let newData = await axios.get(`${process.env.VUE_APP_API_URL}/loans/fetchById/${loan.id}`,{
-                headers: {
-                    token: localStorage.getItem('token')
-                }
-            })
-            .then(res => {
-                return res.data;
-            });
-            pendingLoansData.value.push({reference: loan ,books: newData});
-        }
     })
     .catch(error => {
-        M.toast({html: `Error en la solicitud: ${error.response.data.message}`, classes: 'yellow darken-4'});
+        M.toast({html: `Error en la solicitud: ${error.message}`, classes: 'yellow darken-4'});
     });
 }
-function changeStatusModal(loanId, bookId, cover, title, author, isbn){
-    selLoanId.value = loanId;
-    selBookId.value = bookId;
-    selBookCover.value = cover;
-    selBookTitle.value = title;
-    selBookAuthor.value = author;
-    selBookIsbn.value = isbn;
-    let changeStatusModal = M.Modal.getInstance(document.querySelector('#changeStatusModal'));
-    changeStatusModal.open();
+async function fetchReturned() {
+    returnedLoans.value = [];
+    axios.get(`${process.env.VUE_APP_API_URL}/loans/fetchReturned`, {
+        headers: {
+            token: localStorage.getItem('token')
+        }
+    })
+    .then(res => {
+        returnedLoans.value = res.data;
+    })
+    .catch(error => {
+        M.toast({html: `Error en la solicitud: ${error.message}`, classes: 'yellow darken-4'});
+    });
+}
+function selectBook(loanId, bookId, username, contactNumber, email, date, returnDate, title, author, cover, pending, isbn){
+    selBook.value.loanId = loanId;
+    selBook.value.bookId = bookId;
+    selBook.value.username = username;
+    selBook.value.contactNumber = contactNumber;
+    selBook.value.email = email;
+    selBook.value.date = date;
+    selBook.value.returnDate = returnDate;
+    selBook.value.title = title;
+    selBook.value.author = author;
+    selBook.value.cover = cover;
+    selBook.value.pending = pending;
+    selBook.value.isbn = isbn;
 }
 function confirmChangeStatus(){
-    axios.patch(`${process.env.VUE_APP_API_URL}/loans/update/${selLoanId.value}/${selBookId.value}/1`, null, {
+    axios.patch(`${process.env.VUE_APP_API_URL}/loans/update/${selBook.value.loanId}/${selBook.value.bookId}/1`, null, {
         headers: {
             token: localStorage.getItem('token')
         }
     })
     .then(() => {
         M.toast({html: 'Libro entregado!', classes: 'green lighten-1'});
-        fetchPendingLoans();
-        fetchReturnedLoans();
+        fetchPending();
+        fetchReturned();
+        let changeStatusModal = M.Modal.getInstance(document.querySelector('#changeStatusModal'));
+        changeStatusModal.close();
     })
     .catch(error => {
-        M.toast({html: `Error en la solicitud: ${error.response.data.message}`, classes: 'yellow darken-4'});
+        M.toast({html: `Error en la solicitud: ${error.message}`, classes: 'yellow darken-4'});
     });
 }
 </script>
 
 <style scoped>
-.row{
-    padding-top: 5vh;
-}
 .responsive-table{
     overflow-x: auto;
 }
